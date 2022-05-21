db06b7c4dc4077282c9d65e5f37ef3556da02f38
JackKwok654 <jackkwoktszkin629@gmail.com>
2022-05-21 12:04:28 +0800

    - Added Instrumentation for tracning gql time spent

---
+++ sku/src/main/java/com/jack/sku/config/RequestLoggingInstrumentation.java
@@ -0,0 +1,34 @@
+package com.jack.sku.config;
+
+import graphql.ExecutionResult;
+import graphql.execution.instrumentation.InstrumentationContext;
+import graphql.execution.instrumentation.SimpleInstrumentation;
+import graphql.execution.instrumentation.SimpleInstrumentationContext;
+import graphql.execution.instrumentation.parameters.InstrumentationExecutionParameters;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class RequestLoggingInstrumentation extends SimpleInstrumentation {
+    @Override
+    public InstrumentationContext<ExecutionResult> beginExecution(InstrumentationExecutionParameters parameters) {
+        long startMillis = System.currentTimeMillis();
+        var execution = parameters.getExecutionInput();
+
+        return new SimpleInstrumentationContext<>() {
+            @SneakyThrows
+            @Override
+            public void onCompleted(ExecutionResult executionResult, Throwable t) {
+                long endMillis = System.currentTimeMillis();
+
+                if (t != null) {
+                    log.info("GraphQL {} failed: {}", execution, t.getMessage(), t);
+                } else {
+                    log.info("GraphQL {} completed in {}ms", execution, endMillis - startMillis);
+                }
+            }
+        };
+    }
+}
